d5de6f4dd089b534b5144589b691cb1a7995b2bc
Joe Frikker <joe.frikker@shibumi.com>
2025-05-06 23:19:53 -0400

    rust overlay

--- dot_config/nixpkgs/overlays.nix
+++ dot_config/nixpkgs/overlays.nix
@@ -1,4 +1,50 @@
 [ (import <rust-overlay>)
+(
+    final: prev:
+        {
+        #     vscode-langservers-extracted = 
+        #         prev.buildNpmPackage rec {
+        #           pname = "vscode-langservers-extracted";
+        #           version = "4.8.0";
+
+        #           src = prev.fetchFromGitHub {
+        #             owner = "hrsh7th";
+        #             repo = pname;
+        #             rev = "v${version}";
+        #             hash = "sha256-sGnxmEQ0J74zNbhRpsgF/cYoXwn4jh9yBVjk6UiUdK0=";
+        #           };
+
+        #           npmDepsHash = "sha256-LFWC87Ahvjf2moijayFze1Jk0TmTc7rOUd/s489PHro=";
+
+        #           buildPhase =
+        #             let
+        #               extensions =
+        #                 if prev.stdenv.isDarwin
+        #                 then "${prev.vscodium}/Applications/VSCodium.app/Contents/Resources/app/extensions"
+        #                 else "${prev.vscodium}/lib/vscode/resources/app/extensions";
+        #             in
+        #             ''
+        #               npx babel ${extensions}/css-language-features/server/dist/node \
+        #                 --out-dir lib/css-language-server/node/
+        #               npx babel ${extensions}/html-language-features/server/dist/node \
+        #                 --out-dir lib/html-language-server/node/
+        #               npx babel ${extensions}/json-language-features/server/dist/node \
+        #                 --out-dir lib/json-language-server/node/
+        #               npx babel ${extensions}/markdown-language-features/server/dist/node \
+        #                 --out-dir lib/markdown-language-server/node/
+        #               cp -r ${prev.vscode-extensions.dbaeumer.vscode-eslint}/share/vscode/extensions/dbaeumer.vscode-eslint/server/out \
+        #                 lib/eslint-language-server
+        #               mv lib/markdown-language-server/node/workerMain.js lib/markdown-language-server/node/main.js
+        #             '';
+        #           meta = with prev.lib; {
+        #             description = "HTML/CSS/JSON/ESLint language servers extracted from vscode";
+        #             homepage = "https://github.com/hrsh7th/vscode-langservers-extracted";
+        #             license = licenses.mit;
+        #             maintainers = with maintainers; [ lord-valen ];
+        #           };
+        #         };
+        }
+)
 # (
 #     final: prev:
 #         {
